88c0880d6f06b0d214e732d0ad92ea2002e8122d
Victor <victoriglcue@gmail.com>
2018-10-30 10:59:15 +0100

    Ejercicio 7 terminado. Pasado PEP8

--- client.py
+++ client.py
@@ -6,20 +6,26 @@ Programa cliente UDP que abre un socket a un servidor
 
 import socket
 import sys
-# Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-SIP_DIRECTION = sys.argv[4]
-EXPIRES = 80
+
+try:
+    # Constantes. Dirección IP del servidor y contenido a enviar
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    SIP_DIRECTION = sys.argv[4]
+    EXPIRES = sys.argv[5]
+except:
+    sys.exit('Usage: client.py ip puerto register sip_address expires_value')
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    LINE_ = ("REGISTER " + 'sip:' + SIP_DIRECTION + ' SIP/2.0\\r\\n\\r\\n ')
-    LINE = (LINE_ + '\n ' + 'Expires: ' + str(EXPIRES) + '\\r\\n\\r\\n')
+    LINE_ = ("REGISTER " + 'sip:' + SIP_DIRECTION + ' SIP/2.0\\r\\n\\r\\n\n')
+    LINE = (LINE_ + 'Expires: ' + str(EXPIRES) + '\\r\\n\\r\\n')
+    print('\n')
     print(LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-    print("SIP/2.0 200 OK\\r\\n\\r\\n", data.decode('utf-8'))
+    print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
+print('\n')
--- server.py
+++ server.py
@@ -21,23 +21,25 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         handle method of the server class
         (all requests will be handled by this method)
         """
+        print('\n')
         text = self.rfile.read()
         client_text = text.decode('utf-8').split(' ')
-        # self.wfile.write(b"SIP/2.0 200 OK\\r\\n\\r\\n")
+
         direction_sip = client_text[1][4:]
         print("IP cliente: ", self.client_address[0])
         print("Puerto cliente: ", self.client_address[1])
         dic[direction_sip] = self.client_address[0]
-        expires_time = int(client_text[5][:-10])
+        expires_time = int(client_text[3][:-10])
         # print(expires_time)
         if expires_time == 0:
             del dic[direction_sip]
-        print(dic)
+        if len(dic) == 0:
+            print('Without SIP directions')
+        else:
+            print(dic)
 
-
-
-
-
+        print('\n')
+        self.wfile.write(b"SIP/2.0 200 OK\\r\\n\\r\\n")
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
@@ -47,6 +49,6 @@ if __name__ == "__main__":
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
-        self.wfile.write(b"SIP/2.0 200 OK\\r\\n\\r\\n")
+        # self.wfile.write(b"SIP/2.0 200 OK\\r\\n\\r\\n")
     except KeyboardInterrupt:
         print("Finalizado servidor")
